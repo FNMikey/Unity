b991085fe213a9e3729048f433bbc117ac4bd52c
Michau <72571650+FNMikey@users.noreply.github.com>
2024-04-07 11:48:25 +0200

    Added Isometric camera and wood object

--- Callisto/Assets/Scripts/CameraFollow.cs
+++ Callisto/Assets/Scripts/CameraFollow.cs
@@ -3,22 +3,23 @@
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;        // obiekt, za którym kamera ma podążać
-    public float cameraRotation = 50f; // Nachylenie kamery względem gracza
-    public float cameraYOffset = 5f; // odległość kamery w osi Y względem gracza
-    public float cameraZOffset = -2f; // odległość kamery w osi Z względem gracza
+    //public float cameraRotation = 50f; // Nachylenie kamery względem gracza
+    //public float cameraYOffset = 5f; // odległość kamery w osi Y względem gracza
+    //public float cameraZOffset = -2f; // odległość kamery w osi Z względem gracza
 
     private Vector3 initialPosition;
 
     void Start()
     {
-        initialPosition = transform.position;
+        //initialPosition = transform.position;
     }
 
     void LateUpdate()
     {
-        Vector3 desiredPosition = target.position + new Vector3(0, cameraYOffset, cameraZOffset);
+        //Vector3 desiredPosition = target.position + new Vector3(0, cameraYOffset, cameraZOffset);
+        Vector3 desiredPosition = target.position + new Vector3(0, 5, 0);
         transform.position = desiredPosition;
 
-        transform.rotation = Quaternion.Euler(cameraRotation, 0, 0);
+        //transform.rotation = Quaternion.Euler(cameraRotation, 0, 0);
     }
 }
