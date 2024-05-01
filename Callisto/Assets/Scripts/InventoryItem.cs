bcd75dc9ffa9a95e00ec2290d491596081fbad4e
JohnyRlk <jan.rycielski@gmail.com>
2024-05-01 17:09:10 +0200

    update crafting

--- Callisto/Assets/Scripts/InventoryItem.cs
+++ Callisto/Assets/Scripts/InventoryItem.cs
@@ -63,11 +63,11 @@ InventoryItem
         {
             if (item.stackable && item.itemID == droppedItem.item.itemID)
             {
-                if (count < 4)
+                if (count < 5)
                 {
                     count += droppedItem.count; 
                     RefreshCount();
-                    count = Mathf.Min(count, 4);
+                    count = Mathf.Min(count, 5);
                     Destroy(droppedItem.gameObject);
                 }
             }
